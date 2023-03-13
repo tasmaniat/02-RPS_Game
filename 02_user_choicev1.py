29095295827dba5e1962ed816782612984dc38cb
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2023-03-13 14:48:28 +1300

    Add files via upload

--- 02_user_choicev1.py
+++ 02_user_choicev1.py
@@ -1,19 +1,37 @@
 # Functions go here
 def choice_checker(question):
+
+    error = "Please choose from rock / paper / " \
+            "scissors (or xxx to quit)"
     valid = False
     while not valid:
 
         # Ask user for choice
-        response = input(question)
+        response = input(question).lower()
 
         if response == "r" or response == "rock":
             return response
+        elif response == "p" or response == "paper":
+            return response
+        elif response == "s" or response == "scissors":
+            return response
+
+        # check for exit code...
+        elif response == "xxx":
+            return response
+        else:
+            print(error)
 
 
 # Main routine goes here
 
-# ASk user for choice and check it's valid
-user_choice = choice_checker("Choose rock / paper/"
-                             "scissors (r/p/s):  ")
+# Loop for testing purposes
+user_choice = ""
+while user_choice != "xxx":
 
-# print out choice for comparison purposes
+    # ASk user for choice and check it's valid
+    user_choice = choice_checker("Choose rock / paper/"
+                                 "scissors (r/p/s):  ")
+
+    # print out choice for comparison purposes
+    print("You chose {}".fromat(user_choice))
